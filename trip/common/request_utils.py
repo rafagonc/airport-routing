60c521721cb564f79c26d0529bdbf80b02949935
Rafael <rafagonc77@yahoo.com.br>
2019-04-30 16:25:31 -0300

    Final commit

--- trip/common/request_utils.py
+++ trip/common/request_utils.py
@@ -3,6 +3,6 @@ from .exceptions import MissingRequestDataException
 def get_request_field(data, key, optional=False, default=None):
     value = data.get(key, default)
     if value is None and optional is False:
-        raise MissingRequestDataException()
+        raise MissingRequestDataException(data, key)
     return value
         
